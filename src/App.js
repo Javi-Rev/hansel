af64dfb31b90d7a82aa7aacaffc93bf8fcc13fd0
Todd Makinster <tmakinster@guaranteedrate.com>
2019-11-11 16:15:00 -0600

    Styled Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components'
+
 import './App.css';
 
 function App() {
@@ -14,11 +16,17 @@ function App() {
   );
 }
 
+const RidiculousFrame = styled.div`
+  border: 2px solid white;
+  padding: 1.0rem;
+  border-radius: 1rem;
+`
+
 function Hansel() {
   return (
-    <div>
+    <RidiculousFrame>
       <img src="hansel.png" alt="You can dere-lickt..."/>
-    </div>
+    </RidiculousFrame>
   )
 }
 
